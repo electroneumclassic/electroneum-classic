9cba3273af4863cda86bf664212aa2ea347be4fe
Interchained <president@worldvaporexpo.com>
2018-07-06 06:31:23 -0400

    add seeds

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -133,7 +133,11 @@ namespace nodetool
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.electroneum.com",
+    { "seeds.electroneropulse.com",
+      "seeds.electroneropulse.net",
+      "seeds.electroneropulse.info",
+      "seeds.electroneropulse.org",
+     
     };
 
     bool islimitup=false;
