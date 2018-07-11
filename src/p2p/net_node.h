1f344927ddc99bf2f2cec6b3177e233f50dc71e6
vans163 <vans_163@yahoo.com>
2018-07-10 21:32:55 -0400

    update seed node

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -133,7 +133,7 @@ namespace nodetool
     virtual std::map<std::string, time_t> get_blocked_hosts() { CRITICAL_REGION_LOCAL(m_blocked_hosts_lock); return m_blocked_hosts; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.electroneum.com",
+    { "seeds.electroneum-classic.com",
     };
 
     bool islimitup=false;
